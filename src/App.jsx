45e1c62581b0f06e8210070c20aec1a1b9b4faa4
Vika Mika <oneilcode111@gmail.com>
2025-06-09 01:06:10 +1000

    not found page, questionCard props, dynamic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { MainLayout } from "./components/MainLayout";
 import { HomePage } from "./pages/Homepage";
+import { NotFoundPage } from "./pages/NotFoundPage";
 
 function App() {
  return (
@@ -10,6 +11,9 @@ function App() {
        <Route path='/' element={<HomePage />}/>
        <Route path='/forbidden' element={<div>forbidden</div>}/>
        <Route path='/addquestion' element={<div>question</div>}/>
+       <Route path='/question/:id' element={<div>question PAGE</div>}/>
+
+       <Route path='*' element={<NotFoundPage />}/>
       </Route>
     </Routes>
  </BrowserRouter>
--- src/components/QuestionCard/QuestionCard.jsx
+++ src/components/QuestionCard/QuestionCard.jsx
@@ -1,25 +1,27 @@
 import cls from "./QuestionCard.module.css";
 import { Button } from "../Button";
+import { useNavigate } from "react-router-dom";
 
-export const QuestionCard = () => {
+export const QuestionCard = ({card}) => {
+const navigate = useNavigate()
+
   return (
     <div className={cls.card}>
       <div className={cls.cardLabels}>
-        <div>Level: 1</div>
-        <div>Not completed</div>
+        <div>Level: {card.level}</div>
+        <div>{card.completed ? 'Completed' : 'Not completed'}</div>
       </div>
 
-      <h5 className={cls.cardTitle}>Title</h5>
+      <h5 className={cls.cardTitle}>{card.question}</h5>
 
       <div className={cls.cardAnswers}>
         <label>Short answer</label>
         <p className={cls.cardAnswer}>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores,
-          iste?
+        {card.answer}
         </p>
       </div>
 
-        <Button>View</Button>
+        <Button onClick={() => navigate(`/question/${card.id}`)}>View</Button>
     </div>
   );
 };
