cadbd9b1d49cd0657ea97b8ed4f96c860835baa2
Vika Mika <oneilcode111@gmail.com>
2025-06-09 20:58:30 +1000

    icons and searchInput

--- src/pages/Homepage/HomePage.jsx
+++ src/pages/Homepage/HomePage.jsx
@@ -1,35 +1,37 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect} from "react";
 import { API_URL } from "../../constants";
 import { QuestionCardList } from "../../components/QuestionCardList";
 import { Loader } from "../../components/Loader";
 import { useFetch } from "../../hooks/usefetch";
+import { SearchInput } from "../../components/SearchInput/SearchInput";
+import cls from "./Homepage.module.css";
 
 export const HomePage = () => {
   const [questions, setQuestions] = useState([]);
-  const [searchValue, setSearchValue] = useState('');
+  const [searchValue, setSearchValue] = useState("");
 
-  const [getQuestions, isLoading, error] =  useFetch(async (url) => {
+  const [getQuestions, isLoading, error] = useFetch(async (url) => {
     const response = await fetch(`${API_URL}/${url}`);
     const questions = await response.json();
 
-    setQuestions(questions)
-    return questions
-  })
+    setQuestions(questions);
+    return questions;
+  });
 
   useEffect(() => {
-    getQuestions('react');
+    getQuestions("react");
   }, []);
 
-
   const onSearchChangeHandler = (e) => {
-    setSearchValue(e.target.value)
-  }
+    setSearchValue(e.target.value);
+  };
 
   return (
     <>
+      <div className={cls.controlsContainer}>
+      <SearchInput value={searchValue} onChange={onSearchChangeHandler}/>
+      </div>
 
-    <input type="text" value={searchValue} onChange={onSearchChangeHandler}/>
- 
       {isLoading && <Loader />}
       {error && <p>{error}</p>}
       <QuestionCardList cards={questions} />
