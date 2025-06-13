fbacebb60955ff9c93fb32c4777d5ecdae754bf4
Vika Mika <oneilcode111@gmail.com>
2025-06-14 04:09:58 +1000

    remove button

--- src/pages/EditQuestionPage/EditQuestion.jsx
+++ src/pages/EditQuestionPage/EditQuestion.jsx
@@ -4,8 +4,10 @@ import { Loader } from "../../components/Loader";
 import { QuestionForm } from "../../components/QuestionForm";
 import { delayFn } from "../../helpers/delay";
 import { API_URL } from "../../constants";
-import { toast } from 'react-toastify'
+import { toast } from "react-toastify";
 import { dateFormat } from "../../helpers/dateFormat";
+import { useFetch } from "../../hooks/usefetch";
+import { useNavigate } from "react-router-dom";
 
 const editCardAction = async (_prevState, formData) => {
   try {
@@ -13,7 +15,7 @@ const editCardAction = async (_prevState, formData) => {
 
     const newQuestion = Object.fromEntries(formData);
     const resources = newQuestion.resourses.trim();
-    const questionId = newQuestion.questionId
+    const questionId = newQuestion.questionId;
     const isClearForm = newQuestion.clearForm;
 
     const response = await fetch(`${API_URL}/react/${questionId}`, {
@@ -25,7 +27,7 @@ const editCardAction = async (_prevState, formData) => {
         resources: resources.length ? resources.split(",") : [],
         level: Number(newQuestion.level),
         completed: false,
-        editDate: dateFormat(new Date( )),
+        editDate: dateFormat(new Date()),
       }),
     });
 
@@ -44,21 +46,46 @@ const editCardAction = async (_prevState, formData) => {
   }
 };
 
-
 export const EditQuestion = ({ initialState = {} }) => {
-  const [formState, formAction, isPending] = useActionState(editCardAction, {...initialState, clearForm: false});
+  const navigate = useNavigate()
+  const [formState, formAction, isPending] = useActionState(editCardAction, {
+    ...initialState,
+    clearForm: false,
+  });
+
+  const [removeQuestion, isQuestionRemoving] = useFetch(async () => {
+    await fetch(`${API_URL}/react/${initialState.id}`, {
+      method: "DELETE",
+    });
+
+    toast.success("The question was removed");
+    navigate("/")
+  });
+
+  const onRemoveQuestionHandler = () => {
+    const isRemove = confirm("Are you sure?")
+    isRemove && removeQuestion()
+  }
 
   return (
     <>
-      {isPending && <Loader />}
+      {(isPending || isQuestionRemoving) && <Loader />}
 
       <h1 className={cls.formTitle}>Edit question</h1>
 
       <div className={cls.formContainer}>
+        <button
+          className={cls.removeBtn}
+          disabled={isPending || isQuestionRemoving}
+          onClick={onRemoveQuestionHandler}
+        >
+          X
+        </button>
+
         <QuestionForm
           formAction={formAction}
           formState={formState}
-          isPending={isPending}
+          isPending={isPending || isQuestionRemoving}
           submitBtnTest="Edit question"
         />
       </div>
