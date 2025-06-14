aaa3f957574d1d080c60d30755f8f971306e00a1
Vika Mika <oneilcode111@gmail.com>
2025-06-15 01:09:08 +1000

    theme toggler logic

--- src/features/ThemeToggler/ThemeToggler.jsx
+++ src/features/ThemeToggler/ThemeToggler.jsx
@@ -3,18 +3,25 @@ import { useTheme } from "../../hooks/useTheme";
 import cls from "./ThemeToggler.module.css";
 
 export const ThemeToggler = () => {
-const { theme, setTheme } = useTheme()
+  const { theme, setTheme } = useTheme();
 
-const onChangeHandler = (e) => {
-const updatedTheme = e.target.checked === false ? 'light' : 'dark'
-  setTheme(updatedTheme)
-  
-  localStorage.setItem(THEME_STORAGE, updatedTheme)
-}
+  const onChangeHandler = (e) => {
+    const isChecked = e.target.checked === true;
+    const updatedTheme = isChecked ? "dark" : "light";
+
+    setTheme(updatedTheme);
+    isChecked ? document.body.classList.add("darkLayout") : document.body.classList.remove("darkLayout")
+
+    localStorage.setItem(THEME_STORAGE, updatedTheme);
+  };
 
   return (
-    <div className={cls.toggle} >
-      <input type="checkbox" onChange={onChangeHandler} checked={theme === 'dark'}/>
+    <div className={cls.toggle}>
+      <input
+        type="checkbox"
+        onChange={onChangeHandler}
+        checked={theme === "dark"}
+      />
       <label></label>
     </div>
   );
--- src/theme/ThemeProvider.jsx
+++ src/theme/ThemeProvider.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useLayoutEffect, useState } from "react";
 import { THEME_STORAGE } from "../constants";
 
 export const ThemeContext = createContext(null);
@@ -6,6 +6,30 @@ export const ThemeContext = createContext(null);
 export const ThemeProvider = ({ children }) => {
   const savedTheme = localStorage.getItem(THEME_STORAGE) || "light";
   const [theme, setTheme] = useState(savedTheme);
+
+  useLayoutEffect(() => {
+    const detectTheme = () => {
+        const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches
+
+        if(isDark) {
+            setTheme("dark")
+            document.body.classList.remove("darkLayout")
+        } else {
+            savedTheme === "dark" && document.body.classList.add("darkLayout")
+            setTheme(savedTheme)
+        }
+    }
+
+    detectTheme()
+
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+    mediaQuery.addEventListener("change", detectTheme);
+
+    return () => {
+      mediaQuery.removeEventListener("change", detectTheme);
+    };
+  }, []);
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
